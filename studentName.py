3ab8e2b99b7ca8fd50f67b17dc595c645f550a41
Eliza <ekoknapp@gmail.com>
2021-09-21 21:04:33 -0400

    python program from class

---
+++ studentName.py
@@ -0,0 +1,18 @@
+# Knapp Eliza
+# SoftDev
+# Print A SoftDev Student's Name
+# 2021-09-21
+
+import random
+
+def generateName(filename):
+  names = []
+  file = open(filename)
+  for line in file:
+    names.append(line.strip())
+  # print(names)
+
+  random.shuffle(names)
+  return names[0]
+
+print(generateName("names.txt"))
