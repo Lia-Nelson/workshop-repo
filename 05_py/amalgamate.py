7d19bc6792c55fb2171bea3b9fd22caf8e7e7d73
Eliza <ekoknapp@gmail.com>
2021-09-26 14:35:54 -0400

    added try catch blocks for exceptions:

--- 05_py/amalgamate.py
+++ 05_py/amalgamate.py
@@ -5,15 +5,38 @@
 
 import random
 
-def generateName(filename):
-  names = []
-  file = open(filename)
-  for line in file:
-    names.append(line.strip())
-  # print(names)
-  file.close()
+def generate_name(pd1_file, pd2_file):
+  pd1 = []
+  pd2 = []
 
-  random.shuffle(names)
-  return names[0]
+  try:
+    pd1_names = open(pd1_file)
+    for line in pd1_names:
+      pd1.append(line.strip())
+    pd1_names.close()
 
-print(generateName("names.txt"))
+    try: 
+      random.shuffle(pd1)
+      print("pd1: " + pd1[0])
+      
+    except:
+      print("Pd1 text file is empty")
+  except:
+    print("The pd1 file doesn't exist")
+  
+  try:
+    pd2_names = open(pd2_file)
+    for line in pd2_names:
+      pd2.append(line.strip())
+    pd2_names.close()
+
+    try: 
+      random.shuffle(pd2)
+      print("pd2: " + pd2[0])
+
+    except:
+      print("Pd2 text file is empty")
+  except:
+    print("The pd2 file doesn't exist")
+
+generate_name("pd1.txt", "pd2.txt")
