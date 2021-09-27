ac507614013fecebcba109a0af4f20e560fc833b
Eliza <ekoknapp@gmail.com>
2021-09-27 09:25:39 -0400

    new format based on instructions

--- 05_py/amalgamate.py
+++ 05_py/amalgamate.py
@@ -5,6 +5,8 @@
 
 '''
 Summary: 
+
+FIRST ATTEMPT
 After looking at all three of our files, we decided that it would be easiest
 to read the names off of two separate text files, one for period 1 and the 
 other for period 2. Our generate_name function takes in each of those files
@@ -13,9 +15,14 @@ contents to a list and then shuffles it and prints the first index. If opening
 the files or looking at their content is not possible, it prints the according
 error message.
 
+GIVEN NEW DIRECTIONS
+We have a dictionary with a list for each period. Based on the length of the 
+lists, we choose a random index and print the corresponding name in the list.
+
 Discoveries:
 - we learned how to use try: except: in python (because none of us took year 
 long introCS)
+- we learned what a dictionary was and how to access keys and its items.
 
 Questions:
 - we discussed whether to print the outputs inside the function or whether to
@@ -26,43 +33,29 @@ Comments:
 - although it is faster to generate a random integer and take that index of the 
 array instead of shuffling, we ended up going with random.shuffle(array) because
 it looks prettier in the code and the lists aren't too long.
+- UPDATE: we are now generating a random integer
 
 '''
 
 import random
 
-def generate_name(pd1_file, pd2_file):
-  pd1 = []
-  pd2 = []
+def generate_name():
+  NAMES = {
+    "pd1": ["Emma Buller", "Julia Nelson", "Owen Yaggy", "Haotian Gao", "Reng Zheng", "Kevin Cao", "Michelle Lo", "Ivan Lam",
+    "Christopher Liu", "Lucas Lee"],
+    "pd2": ["Jesse Xie", "Rachel Xiao", "Yuqing Wu", "Liesel Wong", "Josephine Lee",
+    "Yaying Liang", "Justin Zou", "Patrick Ging", "Raymont Yueng", "Michael Borczuk",
+    "Eliza Knapp", "Thomas Yu", "Jonathan Wu", "Andy Lin", "Andrew Juang", "Annabel Zhang",
+    "David Chong"]
+  }
 
-  try:
-    pd1_names = open(pd1_file)
-    for line in pd1_names:
-      pd1.append(line.strip())
-    pd1_names.close()
+  # here we pick a random index based on the size of the lists
+  index1 = random.randint(0, len(NAMES["pd1"])-1)
+  index2 = random.randint(0, len(NAMES["pd2"])-1)
 
-    try: 
-      random.shuffle(pd1)
-      print("pd1: " + pd1[0])
-      
-    except:
-      print("Pd1 text file is empty")
-  except:
-    print("The pd1 file doesn't exist")
-  
-  try:
-    pd2_names = open(pd2_file)
-    for line in pd2_names:
-      pd2.append(line.strip())
-    pd2_names.close()
+  # print the names and period clarification
+  print("pd1: " + (NAMES["pd1"])[index1])
+  print("pd2: " + (NAMES["pd2"])[index2])
 
-    try: 
-      random.shuffle(pd2)
-      print("pd2: " + pd2[0])
 
-    except:
-      print("Pd2 text file is empty")
-  except:
-    print("The pd2 file doesn't exist")
-
-generate_name("pd1.txt", "pd2.txt")
+generate_name()
