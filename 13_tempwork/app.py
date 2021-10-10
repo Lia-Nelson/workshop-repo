5079ad397613986e50f1e7e5885391c4e3ff7e1f
Eliza <ekoknapp@gmail.com>
2021-10-10 13:48:27 -0400

    added links but now chosen thing is not working

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -3,41 +3,24 @@
 # K13 -- Template for Success
 # 2021-10-08
 
-from flask import Flask, render_template
-import csv
-import random
+from flask import Flask, render_template, redirect
+import functions
 
 app = Flask(__name__)
 
-with open("./data/occupations.csv") as csvfile:
-    # read the occupations.csv into a reader file, which contains a dict of each line
-    reader = csv.DictReader(csvfile)
-
-    # these will store the job classes and percentages
-    job_class = []
-    percentage = []
-
-    # iterates through each line, and fills job_class and percentage with the correct values
-    for line in reader:
-        job_class.append(line["Job Class"])
-        percentage.append(float(line["Percentage"]))
-
-    # removing last item (because it's Total : 99.8)
-    job_class.pop(len(job_class) - 1)
-    percentage.pop(len(percentage) - 1)
-  
-def occupations(): #edited to return instead of print
-    # job_class is what we want to pick weights are the percentages and we only want to pick once (k=1) then we print whatever job class it was
-    randomList = random.choices(job_class, weights=percentage, k=1)
-    return(randomList[0])
-
 @app.route("/")
 def hello_world():
-    return "Hi!"
+    return redirect("/occupyflaskst")
 
 @app.route("/occupyflaskst")
 def test_tmplt():
-    return render_template( 'tablified.html', chosenJob=occupations(), jobClass=job_class, percentage=percentage)
+    # read in csv file to "occupations" dictionary
+    occupations = functions.readfile("./data/occupations.csv")
+    # choose random occupation
+    chosen_occupation = functions.random_occupation(occupations)
+
+    return render_template('tablified.html', chosenJob=chosen_occupation, jobClass=occupations)
+
 
 if __name__ == "__main__":
     app.debug = True
---
+++ 13_tempwork/functions.py
@@ -0,0 +1,38 @@
+# Hedgehogs (Andrew Juang, David Chong, Eliza Knapp)
+# SoftDev
+# K13 -- Template for Success
+# 2021-10-08
+
+import csv
+import random
+
+def readfile(filepath):
+    # declaration of dictionary to hold occupations + percentages
+    occupations_dict = {}
+
+    # open/read csv file
+    with open(filepath) as csvfile:
+        # read the occupations.csv into a reader file, which contains a dict of each line
+        reader = csv.DictReader(csvfile)
+        # add dictionary items
+        for line in reader:
+            percent_link = []
+            percent_link.append(float(line['Percentage']))
+            percent_link.append(line['Link'])
+            occupations_dict[line['Job Class']] = percent_link
+
+    # remove unnecessary item
+    occupations_dict.pop('Total')
+    print("afjds")
+
+    return occupations_dict
+
+
+def random_occupation(occupations):
+    # choose random occupation given weights
+    occupation_list = list(occupations.keys())
+    # weights = occupations.values()
+    # choice = random.choices(occupation_list, weights)
+
+    # return(choice[0])
+    return 5
